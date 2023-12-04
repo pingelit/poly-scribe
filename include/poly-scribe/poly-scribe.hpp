b8f91129dfd884bdbd4b5faf0a1617fd0f47737f
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2023-12-04 14:29:59 +0100

    Feat: add main header file for the library

---
+++ include/poly-scribe/poly-scribe.hpp
@@ -0,0 +1,15 @@
+/**
+ * \file poly-scribe.hpp
+ * \brief Main header for the poly-scribe library.
+ *
+ * This includes the other header for the library.
+ * \author Pascal Palenda ppa@akustik.rwth-aachen.de
+ * \copyright
+ * Copyright (c) 2023-present Pascal Palenda
+ * Distributed under the MIT License (http://opensource.org/licenses/MIT)
+ */
+
+#ifndef POLY_SCRIBE_POLY_SCRIBE_HPP
+#define POLY_SCRIBE_POLY_SCRIBE_HPP
+
+#endif
