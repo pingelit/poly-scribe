bc1694511c37c953c5954bf2742e8b71c9511908
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2023-12-07 20:58:18 +0100

    Test: add first test for ScribeWrapper

--- test/scribe-wrapper.cpp
+++ test/scribe-wrapper.cpp
@@ -1,6 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
+#include <poly-scribe/poly-scribe.hpp>
 
-TEST_CASE( "Dummy", "[dummy]" )
+TEST_CASE( "scribe-wrapper::base", "[scribe-wrapper]" )
 {
-	REQUIRE( true );
+	using namespace poly_scribe;
+	SECTION( "Construction" )
+	{
+		int integer = 0;
+
+		auto wrap = make_scribe_wrap( "name", integer );
+	}
 }
