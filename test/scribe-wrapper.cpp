989129f84c489b9da3f1dd012f1a22b88c4dc71e
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2023-12-27 12:44:54 +0100

    Test: improve template test
    - add string
    - check that we can change the value

--- test/scribe-wrapper.cpp
+++ test/scribe-wrapper.cpp
@@ -6,16 +6,53 @@
 #include <cereal/archives/json.hpp>
 #include <cereal/archives/xml.hpp>
 #include <poly-scribe/poly-scribe.hpp>
+#include <string>
 
-TEMPLATE_TEST_CASE( "scribe-wrapper::base", "[scribe-wrapper][template]", bool, int, char, float, double, long )
+TEMPLATE_TEST_CASE( "scribe-wrapper::base", "[scribe-wrapper][template]", bool, char, int, float, double, long, std::string )
 {
 	using namespace poly_scribe;
-	SECTION( "Construction" )
+	TestType value{};
+
+	if constexpr( std::is_same_v<bool, TestType> )
 	{
-		TestType integer = 0;
-		auto wrap        = make_scribe_wrap( "name", integer );
-		REQUIRE( true );
+		value = GENERATE( true, false );
 	}
+	else if constexpr( std::is_same_v<char, TestType> )
+	{
+		value = GENERATE( take( MAX_REPS, random( -128, 127 ) ) );
+	}
+	else if constexpr( std::is_same_v<std::string, TestType> )
+	{
+		value = GENERATE_RANDOM_STRING( 10 );
+	}
+	else if constexpr( !std::is_same_v<char, TestType> && !std::is_same_v<bool, TestType> && !std::is_same_v<std::string, TestType> )
+	{
+		value = GENERATE_RANDOM( TestType, MAX_REPS );
+	}
+
+	auto name = GENERATE_RANDOM_STRING( 10 );
+	auto wrap = make_scribe_wrap( name, value );
+	REQUIRE( wrap.m_name == name );
+	REQUIRE( wrap.m_value == value );
+
+	if constexpr( std::is_same_v<bool, TestType> )
+	{
+		value = GENERATE( true, false );
+	}
+	else if constexpr( std::is_same_v<char, TestType> )
+	{
+		value = GENERATE( take( MAX_REPS, random( -128, 127 ) ) );
+	}
+	else if constexpr( std::is_same_v<std::string, TestType> )
+	{
+		value = GENERATE_RANDOM_STRING( 10 );
+	}
+	else if constexpr( !std::is_same_v<char, TestType> && !std::is_same_v<bool, TestType> && !std::is_same_v<std::string, TestType> )
+	{
+		value = GENERATE_RANDOM( TestType, MAX_REPS );
+	}
+
+	REQUIRE( wrap.m_value == value );
 }
 
 TEST_CASE( "scribe-wrapper::base", "[scribe-wrapper]" )
