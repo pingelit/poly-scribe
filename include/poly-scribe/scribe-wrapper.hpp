068dcdaa591cddc496b9c0a4a4235af44dece987
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-01-01 22:45:38 +0100

    Doc: `ScribeWrapper`

--- include/poly-scribe/scribe-wrapper.hpp
+++ include/poly-scribe/scribe-wrapper.hpp
@@ -20,17 +20,36 @@
 
 namespace poly_scribe
 {
+	///
+	/// \brief Wraps a "normal" value to be used with the poly-scribe environment.
+	///
+	/// It holds a reference to a value that will be serialized using the cereal archives.
+	/// This basically is a cereal::NameValuePair but also is the basis for the poly-scribe eco system.
+	/// It should be mainly created using the ::make_scribe_wrap function.
+	/// \tparam T type of the value the wrapper holds.
+	///
 	template<typename T>
 	class ScribeWrapper
 	{
+		///
+		/// \brief Get the correct type to hold.
+		///
+		/// See cereal::NameValuePair for more info.
+		///
 		using Type = typename std::conditional<std::is_array<typename std::remove_reference<T>::type>::value, typename std::remove_cv<T>::type,
 		                                       typename std::conditional<std::is_lvalue_reference<T>::value, T, typename std::decay<T>::type>::type>::type;
 
 	public:
 		// NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
-		Type m_value;
-		std::string m_name;
+		Type m_value;       ///< Wrapped value.
+		std::string m_name; ///< Name used to serialize the value.
 
+		///
+		/// \brief Construct a ScribeWrapper object
+		///
+		/// \param t_value rvalue reference to the value to wrap.
+		/// \param t_name name the value should to be serialized with.
+		///
 		ScribeWrapper( T &&t_value, std::string t_name ) : m_value( std::forward<T>( t_value ) ), m_name( std::move( t_name ) ) {}
 
 		~ScribeWrapper( )                                     = default;
@@ -39,18 +58,36 @@ namespace poly_scribe
 		ScribeWrapper &operator=( ScribeWrapper const & )     = delete;
 		ScribeWrapper &operator=( ScribeWrapper && ) noexcept = delete;
 
+		///
+		/// \brief Save method for the object.
+		/// \remark Not specialized for any specific archive.
+		/// \tparam Archive archive type to save to.
+		/// \param t_archive archive to save to.
+		///
 		template<class Archive>
 		void CEREAL_SAVE_FUNCTION_NAME( Archive &t_archive ) const
 		{
 			t_archive( cereal::make_nvp( m_name, m_value ) );
 		}
 
+		///
+		/// \brief Load method for the object.
+		/// \remark Not specialized for any specific archive.
+		/// \tparam Archive archive type to load to.
+		/// \param t_archive archive to load from.
+		///
 		template<class Archive>
 		void CEREAL_LOAD_FUNCTION_NAME( Archive &t_archive )
 		{
 			t_archive( cereal::make_nvp( m_name, m_value ) );
 		}
 
+		///
+		/// \brief Save method for the object.
+		/// \remark Specialized for the JSON archive in order to always have inline serialization.
+		/// \tparam Archive archive type to save to.
+		/// \param t_archive archive to save to.
+		///
 		template<class T>
 		inline void CEREAL_SAVE_FUNCTION_NAME( cereal::JSONOutputArchive &t_archive )
 		{
@@ -58,6 +95,11 @@ namespace poly_scribe
 			t_archive( m_value );
 		}
 
+		///
+		/// \brief Load method for the object.
+		/// \remark Specialized for the JSON archive in order to always have inline serialization.
+		/// \param t_archive archive to load from.
+		///
 		template<class T>
 		inline void CEREAL_LOAD_FUNCTION_NAME( cereal::JSONInputArchive &t_archive )
 		{
@@ -66,6 +108,10 @@ namespace poly_scribe
 		}
 	};
 
+	///
+	/// \brief Pro- and epilogue functions to ensure that the wrapper is serialized inline for JSON archives.
+	/// \{
+	///
 	template<class T>
 	inline void prologue( cereal::JSONOutputArchive &, ScribeWrapper<T> const & )
 	{
@@ -85,6 +131,7 @@ namespace poly_scribe
 	inline void epilogue( cereal::JSONInputArchive &, ScribeWrapper<T> const & )
 	{
 	}
+	/// \}
 
 	template<typename T>
 	class ScribePointerWrapper
