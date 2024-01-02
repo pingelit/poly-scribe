e445a04f0f61c5419e4695b086a8b32948d90d05
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-01-02 14:41:03 +0100

    Doc: `detail::PolyScribeWrapper`

--- include/poly-scribe/detail/poly-wrapper.hpp
+++ include/poly-scribe/detail/poly-wrapper.hpp
@@ -15,6 +15,14 @@
 
 namespace poly_scribe::detail
 {
+	///
+	/// \brief Thin wrapper used for the polymorphic binding logic.
+	///
+	/// This is used to cast to and from the correct type.
+	/// \tparam T type of the wrapped value.
+	/// \tparam P type of the expected return value.
+	/// \todo check if this class is necessary.
+	///
 	template<typename T, typename P>
 	struct PolyScribeWrapper
 	{
@@ -22,20 +30,30 @@ namespace poly_scribe::detail
 		using Type = typename std::conditional<std::is_array<typename std::remove_reference<T>::type>::value, typename std::remove_cv<T>::type,
 		                                       typename std::conditional<std::is_lvalue_reference<T>::value, T, typename std::decay<T>::type>::type>::type;
 
-
 	public:
 		PolyScribeWrapper( char const *t_name, T &&t_value ) : name( t_name ), m_value( std::forward<T>( t_value ) ) {}
 
-		// todo check against clang-tidy and see if rule of 5 is fulfilled.
+		/// \brief Rule of five methods.
+		/// \todo check against clang-tidy and see if rule of 5 is fulfilled.
+		/// \todo check if we even have to add any of these.
+		/// \{
 		~PolyScribeWrapper( )                                         = default;
 		PolyScribeWrapper( const PolyScribeWrapper & )                = default;
 		PolyScribeWrapper( PolyScribeWrapper && ) noexcept            = default;
 		PolyScribeWrapper &operator=( PolyScribeWrapper && ) noexcept = default;
 		PolyScribeWrapper &operator=( PolyScribeWrapper const & )     = delete;
+		/// \}
 
-		char const *name;
-		Type m_value;
+		char const *name; ///< name of the value \todo this is not used!
+		Type m_value;     ///< wrapped value.
 
+		///
+		/// \brief Save method for the object.
+		/// \tparam Archive archive type to save to.
+		/// \param t_archive archive to save to.
+		/// \todo make the type string a variable, many other places as well.
+		/// \todo inline serialization, to make sure it works for all user types.
+		///
 		template<class Archive>
 		void CEREAL_SAVE_FUNCTION_NAME( Archive &t_archive ) const
 		{
@@ -43,6 +61,12 @@ namespace poly_scribe::detail
 			const_cast<P &>( *m_value ).CEREAL_SERIALIZE_FUNCTION_NAME( t_archive );
 		}
 
+		///
+		/// \brief Load method for the object.
+		/// \tparam Archive archive type to load from.
+		/// \param t_archive archive to load from.
+		/// \todo inline serialization, to make sure it works for all user types.
+		///
 		template<class Archive>
 		void CEREAL_LOAD_FUNCTION_NAME( Archive &t_archive )
 		{
@@ -50,6 +74,14 @@ namespace poly_scribe::detail
 		}
 	};
 
+	///
+	/// \brief Factory for the PolyScribeWrapper.
+	/// \tparam P type of the expected return value.
+	/// \tparam T type of the wrapped value.
+	/// \param t_name name of the wrapped value. Not used in the wrapper!
+	/// \param t_value value to wrap.
+	/// \return the wrapper.
+	///
 	template<typename P, class T>
 	inline PolyScribeWrapper<T, P> make_poly_wrapper( std::string const &t_name, T &&t_value )
 	{
