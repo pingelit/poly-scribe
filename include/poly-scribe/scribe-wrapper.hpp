ba9b0870e5d6ac3505682422313f25c0db2dd9cf
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-01-07 00:59:42 +0100

    Feat: first draft container wrapper

--- include/poly-scribe/scribe-wrapper.hpp
+++ include/poly-scribe/scribe-wrapper.hpp
@@ -15,6 +15,8 @@
 
 #include <cereal/archives/json.hpp>
 #include <cereal/cereal.hpp>
+#include <cereal/types/list.hpp>
+#include <cereal/types/vector.hpp>
 #include <string>
 #include <type_traits>
 
@@ -104,6 +106,134 @@ namespace poly_scribe
 
 	template<class T>
 	inline void epilogue( cereal::JSONInputArchive & /*unused*/, ScribeWrapper<T> const & /*unused*/ )
+	{
+	}
+	/// \}
+
+	template<typename T>
+	class ScribeContainerWrapper
+	{
+		///
+		/// \brief Get the correct type to hold.
+		///
+		/// See cereal::NameValuePair for more info.
+		///
+		using Type = typename std::conditional<std::is_array<typename std::remove_reference<T>::type>::value, typename std::remove_cv<T>::type,
+		                                       typename std::conditional<std::is_lvalue_reference<T>::value, T, typename std::decay<T>::type>::type>::type;
+
+		///
+		/// \brief Helper wrapper for containers.
+		/// \todo Find out, if this class is really necessary.
+		///
+		struct Wrapper
+		{
+			// NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
+			Type &m_value; ///< wrapped value.
+
+			///
+			/// \brief Construct a new Wrapper object
+			/// \param t_value value to wrap.
+			///
+			Wrapper( Type &&t_value ) : m_value( t_value ) {}
+
+			///
+			/// \brief Save method for the object.
+			/// \tparam Archive archive type to save to.
+			/// \param t_archive archive to save to.
+			///
+			template<class Archive>
+			void CEREAL_SAVE_FUNCTION_NAME( Archive &t_archive ) const
+			{
+				t_archive( cereal::make_size_tag( m_value.size( ) ) );
+
+				for( auto const &value: m_value )
+				{
+					t_archive( make_scribe_wrap( "unused", value ) );
+				}
+			}
+
+			///
+			/// \brief Load method for the object.
+			/// \tparam Archive archive type to load from.
+			/// \param t_archive archive to load from.
+			///
+			template<class Archive>
+			void CEREAL_LOAD_FUNCTION_NAME( Archive &t_archive )
+			{
+				size_t size;
+				t_archive( cereal::make_size_tag( size ) );
+
+				m_value.resize( size );
+
+				for( auto &value: m_value )
+				{
+				}
+			}
+		};
+
+	public:
+		// NOLINTNEXTLINE(cppcoreguidelines-avoid-const-or-ref-data-members)
+		Type m_value;       ///< Wrapped value.
+		std::string m_name; ///< Name used to serialize the value.
+
+		///
+		/// \brief Construct a ScribeContainerWrapper object
+		///
+		/// \param t_value rvalue reference to the value to wrap.
+		/// \param t_name name the value should to be serialized with.
+		///
+		ScribeContainerWrapper( T &&t_value, std::string t_name ) : m_value( std::forward<T>( t_value ) ), m_name( std::move( t_name ) ) {}
+
+		~ScribeContainerWrapper( )                                              = default;
+		ScribeContainerWrapper( const ScribeContainerWrapper & )                = delete;
+		ScribeContainerWrapper( ScribeContainerWrapper && ) noexcept            = delete;
+		ScribeContainerWrapper &operator=( ScribeContainerWrapper const & )     = delete;
+		ScribeContainerWrapper &operator=( ScribeContainerWrapper && ) noexcept = delete;
+
+		///
+		/// \brief Save method for the object.
+		/// \tparam Archive archive type to save to.
+		/// \param t_archive archive to save to.
+		///
+		template<class Archive>
+		void CEREAL_SAVE_FUNCTION_NAME( Archive &t_archive ) const
+		{
+			t_archive( cereal::make_nvp( m_name, Wrapper( m_value ) ) );
+		}
+
+		///
+		/// \brief Load method for the object.
+		/// \tparam Archive archive type to load to.
+		/// \param t_archive archive to load from.
+		///
+		template<class Archive>
+		void CEREAL_LOAD_FUNCTION_NAME( Archive &t_archive )
+		{
+			t_archive( cereal::make_nvp( m_name, Wrapper( m_value ) ) );
+		}
+	};
+
+	///
+	/// \brief Pro- and epilogue functions to ensure that the wrapper is serialized inline for JSON archives.
+	/// \{
+	///
+	template<class T>
+	inline void prologue( cereal::JSONOutputArchive & /*unused*/, ScribeContainerWrapper<T> const & /*unused*/ )
+	{
+	}
+
+	template<class T>
+	inline void prologue( cereal::JSONInputArchive & /*unused*/, ScribeContainerWrapper<T> const & /*unused*/ )
+	{
+	}
+
+	template<class T>
+	inline void epilogue( cereal::JSONOutputArchive & /*unused*/, ScribeContainerWrapper<T> const & /*unused*/ )
+	{
+	}
+
+	template<class T>
+	inline void epilogue( cereal::JSONInputArchive & /*unused*/, ScribeContainerWrapper<T> const & /*unused*/ )
 	{
 	}
 	/// \}
@@ -325,6 +455,16 @@ namespace poly_scribe
 	template<class T>
 	inline typename std::enable_if_t<!std::is_polymorphic_v<typename std::remove_reference_t<T>::element_type>, ScribePointerWrapper<T>> make_scribe_wrap(
 	    const std::string &t_name, T &&t_value, detail::SmartPointerTag /*unused*/ )
+	{
+		return { std::forward<T>( t_value ), t_name };
+	}
+
+	///
+	/// \copybrief make_scribe_wrap( const std::string &t_name, T &&t_value )
+	/// Specialized for container types.
+	///
+	template<class T>
+	inline ScribeContainerWrapper<T> make_scribe_wrap( const std::string &t_name, T &&t_value, detail::DynamicContainerTag /*unused*/ )
 	{
 		return { std::forward<T>( t_value ), t_name };
 	}
