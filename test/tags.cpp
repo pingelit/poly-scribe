08e1e872407ba291b07e5d1fbdab151366cd1659
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-01-06 22:41:09 +0100

    Test: sfinae impls

--- test/tags.cpp
+++ test/tags.cpp
@@ -36,3 +36,49 @@ TEMPLATE_TEST_CASE( "tags", "[tags][template]", bool, char, int, float, double, 
 		STATIC_REQUIRE( std::is_same<poly_scribe::detail::GetWrapperTag<std::unique_ptr<TestType>>::type, poly_scribe::detail::SmartPointerTag>::value );
 	}
 }
+
+TEMPLATE_TEST_CASE( "is_container", "[sfinae][template]", bool, char, int, float, double, long, std::string )
+{
+	SECTION( "Generic type" )
+	{
+		STATIC_REQUIRE( !poly_scribe::detail::is_container_v<TestType> );
+	}
+
+	SECTION( "std::vector" )
+	{
+		STATIC_REQUIRE( poly_scribe::detail::is_container_v<std::vector<TestType>> );
+	}
+
+	SECTION( "std::list" )
+	{
+		STATIC_REQUIRE( poly_scribe::detail::is_container_v<std::list<TestType>> );
+	}
+}
+
+TEMPLATE_TEST_CASE( "is_smart_ptr", "[sfinae][template]", bool, char, int, float, double, long, std::string )
+{
+	SECTION( "Generic type" )
+	{
+		STATIC_REQUIRE( !poly_scribe::detail::is_smart_ptr_v<TestType> );
+	}
+
+	SECTION( "Raw ptr" )
+	{
+		STATIC_REQUIRE( !poly_scribe::detail::is_smart_ptr_v<TestType*> );
+	}
+
+	SECTION( "std::shared_ptr" )
+	{
+		STATIC_REQUIRE( poly_scribe::detail::is_smart_ptr_v<std::shared_ptr<TestType>> );
+	}
+
+	SECTION( "std::weak_ptr" )
+	{
+		STATIC_REQUIRE( poly_scribe::detail::is_smart_ptr_v<std::weak_ptr<TestType>> );
+	}
+
+	SECTION( "std::unique_ptr" )
+	{
+		STATIC_REQUIRE( poly_scribe::detail::is_smart_ptr_v<std::unique_ptr<TestType>> );
+	}
+}
