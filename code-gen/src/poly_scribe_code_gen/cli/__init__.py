3d0e674226176eee332834cac698b0e23865b546
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-01-23 20:26:24 +0100

    Feat: add proof of concept generator app

--- code-gen/src/poly_scribe_code_gen/cli/__init__.py
+++ code-gen/src/poly_scribe_code_gen/cli/__init__.py
@@ -2,12 +2,24 @@
 #
 # SPDX-License-Identifier: MIT
 import argparse
+from pathlib import Path
 
 from poly_scribe_code_gen.__about__ import __version__
+from poly_scribe_code_gen.cpp_gen import generate_cpp
+from poly_scribe_code_gen.parse_idl import parse_idl
 
 
 def poly_scribe_code_gen():
     parser = argparse.ArgumentParser(prog="poly-scribe-code-gen", description="Generate poly-scribe code from WebIDL.")
     parser.add_argument("-v", "--version", action="version", version=__version__)
+    parser.add_argument("input", help="Input WebIDL file to generate code from", type=Path)
+    parser.add_argument("-c", "--cpp", help="Generate C++ code", type=Path, metavar="out")
 
     args = parser.parse_args()
+
+    parsed_idl = parse_idl(args.input)
+
+    if args.cpp:
+        generate_cpp(
+            parsed_idl=parsed_idl, additional_data={"author_name": "test", "namespace": "foobar"}, out_file=args.cpp
+        )
