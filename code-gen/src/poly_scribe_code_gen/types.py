e560583c743ce2fbcdc45c5afa86cbdf36761b2e
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-02-04 00:03:22 +0100

    Feat: add fundamental cpp types

--- code-gen/src/poly_scribe_code_gen/types.py
+++ code-gen/src/poly_scribe_code_gen/types.py
@@ -1,11 +1,19 @@
-cpp_types = [
+integer_types = [
     "bool",
+    "char",
+    "unsigned char",
     "short",
     "unsigned short",
+    "int",
+    "unsigned int",
     "long",
     "unsigned long",
     "long long",
     "unsigned long long",
-    "float",
-    "double",
 ]
+
+floating_point_types = ["float", "double", "long double"]
+
+std_types = ["string"]
+
+cpp_types = integer_types + floating_point_types + std_types
