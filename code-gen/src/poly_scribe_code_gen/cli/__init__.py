b2d3092eb5c57842ab16ca45deb32e8184513866
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-03-16 18:15:58 +0100

    Feat: add python code gen to CLI

--- code-gen/src/poly_scribe_code_gen/cli/__init__.py
+++ code-gen/src/poly_scribe_code_gen/cli/__init__.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from poly_scribe_code_gen.__about__ import __version__
 from poly_scribe_code_gen.cpp_gen import AdditionalData, generate_cpp
 from poly_scribe_code_gen.parse_idl import parse_idl
+from poly_scribe_code_gen.py_gen import generate_python
 
 
 def poly_scribe_code_gen():
@@ -16,6 +17,7 @@ def poly_scribe_code_gen():
     parser.add_argument("-v", "--version", action="version", version=__version__)
     parser.add_argument("input", help="Input WebIDL file to generate code from", type=Path)
     parser.add_argument("-c", "--cpp", help="Generate C++ code", type=Path, metavar="out")
+    parser.add_argument("-p", "--py", help="Generate Python code", type=Path, metavar="out")
     parser.add_argument(
         "-a", "--additional-data", help="Additional data for the generation", type=Path, metavar="data", required=True
     )
@@ -28,7 +30,10 @@ def poly_scribe_code_gen():
         additional_data: AdditionalData = json.load(f)
 
     additional_data["year"] = datetime.datetime.now(tz=datetime.timezone.utc).date().year
-    additional_data["out_file"] = args.cpp.name
+    additional_data["out_file"] = args.cpp.name if args.cpp else args.py.name
 
     if args.cpp:
         generate_cpp(parsed_idl=parsed_idl, additional_data=additional_data, out_file=args.cpp)
+
+    if args.py:
+        generate_python(parsed_idl=parsed_idl, additional_data=additional_data, out_file=args.py)
