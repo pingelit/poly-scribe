28abff59f8269f357c80e0fc4f970bab4844d458
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-06-16 20:54:29 +0200

    Test: cpp to matlab to json with validation

--- test/schema_test.py
+++ test/schema_test.py
@@ -6,6 +6,7 @@ import pytest
 from jsonschema import validate
 
 from utils import gen_random_integration_test
+import shutil
 
 
 @pytest.mark.parametrize("test_num", range(5))
@@ -41,6 +42,58 @@ def test_cpp():
     tmp_file = os.path.join(tmp_dir, "schema_test.json")
 
     subprocess.run([cpp_exe, tmp_file])
+
+    schema_file = os.getenv("SCHEMA_FILE")
+    if schema_file is None:
+        raise Exception("SCHEMA_FILE environment variable is not set")
+
+    with open(schema_file) as f:
+        schema = json.load(f)
+
+    with open(tmp_file) as f:
+        instance = json.load(f)
+
+    validate(instance=instance, schema=schema)
+
+
+@pytest.mark.skipif(
+    shutil.which("matlab") is None,
+    reason="Matlab is not available",
+)
+def test_matlab():
+    matlab_exe = shutil.which("matlab")
+    if matlab_exe is None:
+        raise Exception("Matlab is not available")
+
+    matlab_dir = os.getenv("MATLAB")
+    if matlab_dir is None:
+        raise Exception("MATLAB environment variable is not set")
+    assert os.path.exists(matlab_dir)
+
+    cpp_exe = os.getenv("CPP_EXE")
+    if cpp_exe is None:
+        raise Exception("CPP_EXE environment variable is not set")
+    assert os.path.exists(cpp_exe)
+
+    tmp_dir = os.getenv("TMP_DIR")
+    if tmp_dir is None:
+        raise Exception("TMP_DIR environment variable is not set")
+    assert os.path.exists(tmp_dir)
+
+    tmp_file = os.path.join(tmp_dir, "schema_test_matlab.json")
+    tmp_cpp_file = os.path.join(tmp_dir, "schema_test.json")
+
+    subprocess.run([cpp_exe, tmp_cpp_file])
+
+    matlab_script = f"""
+addpath('{matlab_dir}');
+
+integration_test = IntegrationTest.load_from_file('{tmp_cpp_file}');
+
+integration_test.store_to_file('{tmp_file}');
+"""
+
+    subprocess.run([matlab_exe, "-batch", matlab_script])
 
     schema_file = os.getenv("SCHEMA_FILE")
     if schema_file is None:
