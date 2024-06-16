b02f1d34dd3ed8549b3862bb2e8be8303cb6303c
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2024-06-16 13:04:57 +0200

    Test: cpp schema compatibility

--- test/schema_test.py
+++ test/schema_test.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 
 import pytest
 from jsonschema import validate
@@ -24,3 +25,31 @@ def test_schema(test_num):
         schema = json.load(f)
 
     validate(instance=json.loads(json_data), schema=schema)
+
+
+def test_cpp():
+    cpp_exe = os.getenv("CPP_EXE")
+    if cpp_exe is None:
+        raise Exception("CPP_EXE environment variable is not set")
+    assert os.path.exists(cpp_exe)
+
+    tmp_dir = os.getenv("TMP_DIR")
+    if tmp_dir is None:
+        raise Exception("TMP_DIR environment variable is not set")
+    assert os.path.exists(tmp_dir)
+
+    tmp_file = os.path.join(tmp_dir, "schema_test.json")
+
+    subprocess.run([cpp_exe, tmp_file])
+
+    schema_file = os.getenv("SCHEMA_FILE")
+    if schema_file is None:
+        raise Exception("SCHEMA_FILE environment variable is not set")
+
+    with open(schema_file) as f:
+        schema = json.load(f)
+
+    with open(tmp_file) as f:
+        instance = json.load(f)
+
+    validate(instance=instance, schema=schema)
