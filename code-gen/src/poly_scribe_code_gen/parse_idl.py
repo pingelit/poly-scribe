e01f57df7573a6a97b5c8fa8f3d7b3d5f94928e7
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2025-02-09 15:53:40 +0100

    Refactor: improve variable naming

--- code-gen/src/poly_scribe_code_gen/parse_idl.py
+++ code-gen/src/poly_scribe_code_gen/parse_idl.py
@@ -224,9 +224,9 @@ def _add_comments(idl: str, parsed_idl: dict[str, Any]) -> dict[str, Any]:
             if definition in enum_data:
                 enum_data[definition]["inline_comment"] = comment
 
-            for enum_data in enum_data["values"]:
-                if enum_data["name"] in definition:
-                    enum_data["inline_comment"] = comment
+            for enum_value_data in enum_data["values"]:
+                if enum_value_data["name"] in definition:
+                    enum_value_data["inline_comment"] = comment
 
         for typedef_name, typedef_data in parsed_idl["typedefs"].items():
             if typedef_name in definition:
