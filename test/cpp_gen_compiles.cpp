191fb594f1749106ef077125d6a17dd7ac589cd6
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2025-02-22 22:04:00 +0100

    Feat: add test source file

---
+++ test/cpp_gen_compiles.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch_test_macros.hpp>
+#include <poly-scribe-structs/integration_data.hpp>
+
+
+TEST_CASE( "CppGen_compiles_integration_test", "[CppGen]" )
+{
+	STATIC_REQUIRE( true );
+}
