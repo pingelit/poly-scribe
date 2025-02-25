e704a90106d83500d4ad1598c6afc545dc8c8f54
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2025-02-25 20:22:38 +0100

    Refactor: move helper methods out of method for easier testing

--- code-gen/src/poly_scribe_code_gen/py_gen.py
+++ code-gen/src/poly_scribe_code_gen/py_gen.py
@@ -55,90 +55,14 @@ def _render_template(parsed_idl, additional_data):
 
 
 def _transform_types(parsed_idl):
-    @dataclass
-    class ExtraData:
-        polymorphic: bool = False
-
-    def _polymorphic_transformer(type_name):
-        if type_name in parsed_idl["inheritance_data"]:
-            derived = parsed_idl["inheritance_data"][type_name]
-
-            for derived_type in derived:
-                if derived_type in parsed_idl["inheritance_data"]:
-                    derived.extend(parsed_idl["inheritance_data"][derived_type])
-
-            derived_list = ", ".join(derived)
-            return f"Union[{derived_list}, {type_name}]", ExtraData(polymorphic=True)
-
-        for base_type, derived_types in parsed_idl["inheritance_data"].items():
-            if type_name in derived_types and len(derived_types) > 1:
-                derived = parsed_idl["inheritance_data"][base_type]
-
-                for derived_type in derived:
-                    if derived_type in parsed_idl["inheritance_data"]:
-                        derived.extend(parsed_idl["inheritance_data"][derived_type])
-
-                derived_list = ", ".join(derived)
-                return f"Union[{derived_list}, {type_name}]", ExtraData(polymorphic=True)
-
-        return type_name, ExtraData
-
-    def _transformer(type_input):
-        if type_input["union"]:
-            contained_types = []
-            for contained in type_input["type_name"]:
-                transformed_type, extra_data = _transformer(contained)
-                if extra_data.polymorphic:
-                    msg = "Unions with polymorphic types are not supported"
-                    raise ValueError(msg)
-                contained_types.append(transformed_type)
-            transformed_type = ",".join(contained_types)
-            return f"Union[{transformed_type}]", ExtraData()
-        if type_input["vector"]:
-            transformed_type, extra_data = _transformer(type_input["type_name"][0])
-            for attr in type_input["ext_attrs"]:
-                if attr["name"] == "Size" and attr["rhs"]["type"] == "integer":
-                    size = attr["rhs"]["value"]
-                    return f"Annotated[List[{transformed_type}], Len(min_length={size}, max_length={size})]", extra_data
-            return f"List[{transformed_type}]", extra_data
-        if type_input["map"]:
-            transformed_key_type, extra_data_key = _transformer(type_input["type_name"][0])
-            if extra_data_key.polymorphic:
-                msg = "Maps with polymorphic keys are not supported"
-                raise ValueError(msg)
-
-            transformed_value_type, extra_data_value = _transformer(type_input["type_name"][1])
-            return f"Dict[{transformed_key_type}, {transformed_value_type}]", extra_data_value
-        else:
-            conversion = {
-                "string": "str",
-                "ByteString": "str",
-                "bool": "bool",
-                "float": "float",
-                "double": "float",
-                "long double": "float",
-                "char": "int",
-                "unsigned char": "int",
-                "short": "int",
-                "unsigned short": "int",
-                "int": "int",
-                "unsigned int": "int",
-                "long": "int",
-                "unsigned long": "int",
-                "long long": "int",
-                "unsigned long long": "int",
-            }
-            if type_input["type_name"] in conversion:
-                return conversion[type_input["type_name"]], ExtraData()
-            else:
-                return _polymorphic_transformer(type_input["type_name"])
-
-    for struct in parsed_idl["structs"]:
-        for member in struct["members"]:
-            transformed_type, extra_data = _transformer(member["type"])
+    for struct in parsed_idl["structs"].values():
+        for member in struct["members"].values():
+            transformed_type, extra_data = _transformer(member["type"], parsed_idl)
             member["type"] = transformed_type
             if extra_data.polymorphic:
-                member["type"] = f"Annotated[{member['type']}, Field(discriminator=\"type\")]"
+                member["type"] = (
+                    f"Annotated[{member['type']}, Field(discriminator=\"type\")]"
+                )
 
         for _, derived_types in parsed_idl["inheritance_data"].items():
             if struct["name"] in derived_types:
@@ -155,6 +79,95 @@ def _transform_types(parsed_idl):
                     )
 
     for type_def in parsed_idl["type_defs"]:
-        type_def["type"] = _transformer(type_def["type"])[0]
+        type_def["type"] = _transformer(type_def["type"], parsed_idl)[0]
 
     return parsed_idl
+
+
+@dataclass
+class ExtraData:
+    polymorphic: bool = False
+
+
+def _polymorphic_transformer(type_name, parsed_idl):
+    if type_name in parsed_idl["inheritance_data"]:
+        derived = parsed_idl["inheritance_data"][type_name]
+
+        for derived_type in derived:
+            if derived_type in parsed_idl["inheritance_data"]:
+                derived.extend(parsed_idl["inheritance_data"][derived_type])
+
+        derived_list = ", ".join(derived)
+        return f"Union[{derived_list}, {type_name}]", ExtraData(polymorphic=True)
+
+    for base_type, derived_types in parsed_idl["inheritance_data"].items():
+        if type_name in derived_types and len(derived_types) > 1:
+            derived = parsed_idl["inheritance_data"][base_type]
+
+            for derived_type in derived:
+                if derived_type in parsed_idl["inheritance_data"]:
+                    derived.extend(parsed_idl["inheritance_data"][derived_type])
+
+            derived_list = ", ".join(derived)
+            return f"Union[{derived_list}, {type_name}]", ExtraData(polymorphic=True)
+
+    return type_name, ExtraData
+
+
+def _transformer(type_input, parsed_idl):
+    if type_input["union"]:
+        contained_types = []
+        for contained in type_input["type_name"]:
+            transformed_type, extra_data = _transformer(contained)
+            if extra_data.polymorphic:
+                msg = "Unions with polymorphic types are not supported"
+                raise ValueError(msg)
+            contained_types.append(transformed_type)
+        transformed_type = ",".join(contained_types)
+        return f"Union[{transformed_type}]", ExtraData()
+    if type_input["vector"]:
+        transformed_type, extra_data = _transformer(type_input["type_name"][0])
+        for attr in type_input["ext_attrs"]:
+            if attr["name"] == "Size" and attr["rhs"]["type"] == "integer":
+                size = attr["rhs"]["value"]
+                return (
+                    f"Annotated[List[{transformed_type}], Len(min_length={size}, max_length={size})]",
+                    extra_data,
+                )
+        return f"List[{transformed_type}]", extra_data
+    if type_input["map"]:
+        transformed_key_type, extra_data_key = _transformer(type_input["type_name"][0])
+        if extra_data_key.polymorphic:
+            msg = "Maps with polymorphic keys are not supported"
+            raise ValueError(msg)
+
+        transformed_value_type, extra_data_value = _transformer(
+            type_input["type_name"][1]
+        )
+        return (
+            f"Dict[{transformed_key_type}, {transformed_value_type}]",
+            extra_data_value,
+        )
+    else:
+        conversion = {
+            "string": "str",
+            "ByteString": "str",
+            "bool": "bool",
+            "float": "float",
+            "double": "float",
+            "long double": "float",
+            "char": "int",
+            "unsigned char": "int",
+            "short": "int",
+            "unsigned short": "int",
+            "int": "int",
+            "unsigned int": "int",
+            "long": "int",
+            "unsigned long": "int",
+            "long long": "int",
+            "unsigned long long": "int",
+        }
+        if type_input["type_name"] in conversion:
+            return conversion[type_input["type_name"]], ExtraData()
+        else:
+            return _polymorphic_transformer(type_input["type_name"], parsed_idl)
