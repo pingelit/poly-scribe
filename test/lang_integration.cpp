a8dd83d52b74074c1c983c579fbfbf535aaa6269
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2025-03-11 21:31:15 +0100

    Feat: adapt the exe to the new lib - WIP

--- test/lang_integration.cpp
+++ test/lang_integration.cpp
@@ -1,11 +1,11 @@
 #include "poly-scribe-structs/integration_data.hpp"
 
 #include <array>
-#include <cereal/types/unordered_map.hpp>
-#include <cereal/types/vector.hpp>
 #include <fstream>
 #include <random>
+#include <rfl/json.hpp>
 #include <string>
+
 
 
 constexpr int MAX_STRING_SIZE     = 5;
@@ -27,67 +27,90 @@ std::string random_string( )
 	return result;
 }
 
-void gen_random_base( const std::shared_ptr<integration_space::Base>& t_ptr )
+int random_int( )
+{
+	std::random_device random_distribution;
+	std::mt19937 gen( random_distribution( ) );
+	std::uniform_int_distribution dis( -RAND_LIMIT, RAND_LIMIT );
+	return dis( gen );
+}
+
+double random_double( )
+{
+	std::random_device random_distribution;
+	std::mt19937 gen( random_distribution( ) );
+	std::uniform_real_distribution dis( -RAND_LIMIT_FLOAT, RAND_LIMIT_FLOAT );
+	return dis( gen );
+}
+
+// void gen_random_base( integration_space::Base_t& t_base )
+// {
+// 	std::random_device random_distribution;
+// 	std::mt19937 gen( random_distribution( ) );
+
+// 	std::uniform_int_distribution dis_int( -RAND_LIMIT, RAND_LIMIT );
+// 	std::uniform_real_distribution dis_real( -RAND_LIMIT_FLOAT, RAND_LIMIT_FLOAT );
+
+
+// 	t_base->union_member = ( dis_int( gen ) % 2 == 0 ) ? dis_real( gen ) : dis_int( gen );
+// }
+
+integration_space::Base_t gen_random_derived_one( )
 {
 	std::random_device random_distribution;
 	std::mt19937 gen( random_distribution( ) );
 
 	std::uniform_int_distribution dis_int( -RAND_LIMIT, RAND_LIMIT );
-	std::uniform_real_distribution dis_real( -RAND_LIMIT_FLOAT, RAND_LIMIT_FLOAT );
+
+	integration_space::DerivedOne derived_one { };
+
+	derived_one.union_member = ( dis_int( gen ) % 2 == 0 ) ? random_double( ) : random_int( );
 
 	for( int i = 0; i < 3; ++i )
 	{
-		t_ptr->vec_3d[i] = dis_real( gen );
+		derived_one.vec_3d[i] = random_double( );
 	}
 
-	// t_ptr->union_member = ( dis_int( gen ) % 2 == 0 ) ? dis_real( gen ) : dis_int( gen );
-
-	int str_vec_size = dis_int( gen ) % 3 + 1;
+	int str_vec_size = random_int( ) % 3 + 1;
 	for( int i = 0; i < str_vec_size; ++i )
 	{
-		t_ptr->str_vec.push_back( random_string( ) );
+		derived_one.str_vec.push_back( random_string( ) );
 	}
-}
-
-std::shared_ptr<integration_space::DerivedOne> gen_random_derived_one( )
-{
-	std::random_device random_distribution;
-	std::mt19937 gen( random_distribution( ) );
-
-	std::uniform_int_distribution dis_int( -RAND_LIMIT, RAND_LIMIT );
-
-	auto ptr = std::make_shared<integration_space::DerivedOne>( );
-
-	gen_random_base( ptr );
 
 	std::unordered_map<std::string, std::string> string_map;
 	int string_map_size = dis_int( gen ) % 3 + 1;
 	for( int i = 0; i < string_map_size; ++i )
 	{
-		ptr->string_map[random_string( )] = random_string( );
+		derived_one.string_map[random_string( )] = random_string( );
 	}
 
-	return ptr;
+	return derived_one;
 }
 
-std::shared_ptr<integration_space::DerivedTwo> gen_random_derived_two( )
+integration_space::Base_t gen_random_derived_two( )
 {
-	auto ptr = std::make_shared<integration_space::DerivedTwo>( );
+	integration_space::DerivedTwo derived_two { };
 
-	gen_random_base( ptr );
+	derived_two.union_member = ( random_int( ) % 2 == 0 ) ? random_double( ) : random_int( );
 
-	return ptr;
+	for( int i = 0; i < 3; ++i )
+	{
+		derived_two.vec_3d[i] = random_double( );
+	}
+
+	int str_vec_size = random_int( ) % 3 + 1;
+	for( int i = 0; i < str_vec_size; ++i )
+	{
+		derived_two.str_vec.push_back( random_string( ) );
+	}
+
+	return derived_two;
 }
 
 integration_space::NonPolyDerived gen_random_non_poly_derived( )
 {
-	std::random_device random_distribution;
-	std::mt19937 gen( random_distribution( ) );
-
-	std::uniform_int_distribution dis_int( -RAND_LIMIT, RAND_LIMIT );
-
 	integration_space::NonPolyDerived object { };
-	int value = dis_int( gen );
+	object.value = random_int( );
 	return object;
 }
 
@@ -107,8 +130,7 @@ integration_space::IntegrationTest gen_random_integration_test( )
 	object.object_vec.push_back( gen_random_derived_one( ) );
 	object.object_vec.push_back( gen_random_derived_two( ) );
 
-	object.object_array[0] = gen_random_derived_one( );
-	object.object_array[1] = gen_random_derived_two( );
+	object.object_array = { gen_random_derived_one( ), gen_random_derived_two( ) };
 
 	object.enum_value = ( dis_int( gen ) % 2 == 0 ) ? integration_space::Enumeration::value1 : integration_space::Enumeration::value2;
 
@@ -127,16 +149,7 @@ int main( int argc, char* argv[] )
 
 	auto data = gen_random_integration_test( );
 
-	std::stringstream string_stream;
-	{
-		cereal::JSONOutputArchive archive( string_stream );
-		data.serialize( archive );
-	}
-
-	std::ofstream out_file_stream( argv[1] );
-
-	out_file_stream << string_stream.str( );
-	out_file_stream.close( );
+	rfl::json::save( argv[1], data, rfl::json::pretty );
 
 	return 0;
 }
