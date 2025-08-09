36988464fd84b5b02ebce31e55fca5ce60c168cf
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2025-08-09 10:51:51 +0200

    Test: `false` default value

--- code-gen/tests/py_gen_test.py
+++ code-gen/tests/py_gen_test.py
@@ -607,4 +607,26 @@ def test_render_template_struct_with_empty_type_default() -> None:
             assert 'type: Literal["Foo"] = "Foo"'.replace(" ", "") in struct_body.replace(" ", "")
         elif match[0] == "Bar":
             assert 'type: Literal["Bar"] = "Bar"'.replace(" ", "") in struct_body.replace(" ", "")
-            assert "value: Optional[int] = int()".replace(" ", "") in struct_body.replace(" ", "")
+            assert "value: int = int()".replace(" ", "") in struct_body.replace(" ", "")
+
+
+def test__render_template_boolean_default_value() -> None:
+    idl = """
+dictionary Foo {
+    bool foo = true;
+    bool bar = false;
+};
+"""
+    parsed_idl = _validate_and_parse(idl)
+
+    result = py_gen._render_template(parsed_idl, {"package": "foo"})
+
+    pattern = re.compile(r"class\s+(\w+)\((\w*)\):\s*(.*?)\n\n", re.DOTALL)
+    matches = pattern.findall(result)
+
+    assert len(matches) == 1
+    for match in matches:
+        struct_body = match[2]
+        if match[0] == "Foo":
+            assert "foo: bool = True".replace(" ", "") in struct_body.replace(" ", "")
+            assert "bar: bool = False".replace(" ", "") in struct_body.replace(" ", "")
