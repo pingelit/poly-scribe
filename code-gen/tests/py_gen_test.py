4f90c659a25d2d75763ea0d0ba614f017f3b750b
Pascal Palenda <pascal.palenda@akustik.rwth-aachen.de>
2025-08-19 16:05:37 +0200

    Fix: type error

--- code-gen/tests/py_gen_test.py
+++ code-gen/tests/py_gen_test.py
@@ -115,7 +115,7 @@ def test__transform_types_unknown_type() -> None:
     }
 
     with pytest.raises(ValueError, match="Unknown type:"):
-        py_gen._transformer(type_data, {}, [])
+        py_gen._transformer(type_data, {}, set())
 
 
 def test_render_template_typedefs() -> None:
